b169f1a16cd33f0c72869f38166f57cfc0163ede
WDsagor <sagarwpress@gmail.com>
2022-03-01 01:10:29 +0600

    phone api collected

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 const search = () => {
-        const searchText = document.getElementById('searchInput')
+        const searchText = document.getElementById('searchInput').value;
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
             .then(res => res.json())
-            .then(phoneData => console.log(phoneData.data.slice(0, 20)));
+            .then(phoneData => searchResult(phoneData.data.slice(0, 20)));
     }
     // search()
+const searchResult = (phones) => {
+    // console.log(phones);
+    for (const phone of phones) {
+        console.log(phone.phone_name);
+    }
+}
