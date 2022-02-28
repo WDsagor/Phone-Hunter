9d4a461c78a4a9e10ac1998aab82a33c95c19fdf
WDsagor <sagarwpress@gmail.com>
2022-03-01 02:15:29 +0600

    UI Complet

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
 const search = () => {
-        const searchText = document.getElementById('searchInput').value;
+        const inputField = document.getElementById('searchInput');
+        const searchText = inputField.value;
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
             .then(res => res.json())
             .then(phoneData => searchResult(phoneData.data.slice(0, 20)));
+        inputField.value = '';
     }
     // search()
 const searchResult = (phones) => {
     // console.log(phones);
+    const displayContainer = document.getElementById('Phone-container');
+    displayContainer.textContent = '';
     for (const phone of phones) {
-        console.log(phone.phone_name);
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="card m-3 p-3 border rounded border-secondary" style="width: 18rem;">
+                            <img src="${phone.image}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${phone.phone_name}</h5>
+                                <h6>${phone.brand}Brand</h6>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+        `;
+        displayContainer.appendChild(div);
+        // console.log(phone);
+
     }
+
 }
