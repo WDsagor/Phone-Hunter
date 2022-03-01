1b4edf8067ac6ec4ccd98a28381e909f7916d612
WDsagor <sagarwpress@gmail.com>
2022-03-02 02:59:08 +0600

    due error

--- js/app.js
+++ js/app.js
@@ -34,5 +34,86 @@ const details = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then(detailData => console.log(detailData));
+        .then(detailData => showModal(detailData.data));
+
+
 }
+const showModal =(details)=>{
+    console.log(details)
+    const phoneDetail = document.getElementById('phoneDetail');
+    const div = document.createElement('div');
+        div.classList.add('show-part');
+        div.innerHTML=`
+                        <div class="row">
+                                        <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+                                            <img src="${details.image}" alt="" srcset="">
+                                        </div>
+                                        <div class="col-12 col-lg-6 ">
+                                            <div class="custom-table">
+                                                <table class="table table-success table-striped ">
+                                                    <thead>
+                                                        <tr>
+                                                            <th colspan="2">Details</th>
+                                                            
+                            
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>Model</td>
+                                                            <td>${details.name}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Realse date</td>
+                                                            <td>${details.releaseDate}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th colspan="2">Main Features</th>
+                                                        </tr>
+                                                        <tr>
+                                                            <td >Display Size</td>
+                                                            <td >${details.mainFeatures.displaySize}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td >Chip Set</td>
+                                                            <td >${details.mainFeatures.chipSet}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td >Storage</td>
+                                                            <td >${details.mainFeatures.storage}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td >Memory</td>
+                                                            <td >${details.mainFeatures.memory}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <th colspan="2">Others</th>
+                                                        </tr>
+                                                        <tr>
+                                                            <td >Bluetooth</td>
+                                                            <td >${details.others.Bluetooth}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td >GPS</td>
+                                                            <td >${details.others.GPS}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td >WLAN</td>
+                                                            <td >${details.others.WLAN}</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <span onclick="modalOf()"> X </span>
+                        `
+    phoneDetail.appendChild(div);
+    phoneDetail.style.visibility = "visible";
+   
+};
+const modalOf = () => {
+    const phoneDetail1 = document.getElementById('phoneDetail');
+    phoneDetail1.style.visibility = "hidden";
+    phoneDetail1.innerHTML=""
+};
